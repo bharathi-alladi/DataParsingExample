8ec25f3607c55c6f47ec5e5a3253fdf58d79349e
BharathiAlladi <bharathialladi100@gmail.com>
2019-12-22 19:50:39 +0800

    data edited in editview controller saving to the service

--- DataParsingExample/CustomLibraries/ServiceManager/ServiceManager.swift
+++ DataParsingExample/CustomLibraries/ServiceManager/ServiceManager.swift
@@ -64,4 +64,37 @@ class ServiceManager {
         })
         dataTask.resume()
     }
+    
+    func putModifiedContacts(_ url: String, detailContact: DetailDataModel, onCallBack returnBlock:@escaping ((Bool?, Error?) -> Void) ) {
+        
+        var urlRequest = URLRequest.init(url: URL.init(string: url)!)
+        urlRequest.httpMethod = "PUT"
+        let encoder = JSONEncoder.init()
+        do {
+            urlRequest.httpBody = try encoder.encode(detailContact)
+        } catch {
+            returnBlock(nil, error)
+        }
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let defaultConfiq = URLSessionConfiguration.default
+        let urlSession = URLSession.init(configuration : defaultConfiq)
+        
+        let dataTask = urlSession.dataTask(with: urlRequest) { (responseData, response, error) in
+            
+            if responseData != nil {
+                let decoder = JSONDecoder.init()
+                do {
+                    let detailData =  try decoder.decode(DetailDataModel.self, from: responseData!)
+                    returnBlock(true, nil)
+                } catch {
+                    returnBlock(nil, error)
+                }
+            }
+            else {
+                returnBlock(nil, error)
+            }
+        }
+        dataTask.resume()
+    }
 }
